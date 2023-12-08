792ef4bc5bc1eb883e16708c356a5cd9d1ed32ae
NorielAchero <norielrachero@gmail.com>
2023-12-08 23:56:36 +0800

    Initial commit

---
+++ src/singletonPattern/CentralizedQueuingSystem.java
@@ -0,0 +1,70 @@
+package singletonPattern;
+import java.util.Random;
+
+public class CentralizedQueuingSystem {
+    Random random = new Random();
+    private static CentralizedQueuingSystem instance;
+    private int helpDeskACurrentQueueNumber;
+    private int helpDeskBCurrentQueueNumber;
+    private int helpDeskCCurrentQueueNumber;
+
+
+    public CentralizedQueuingSystem() {
+        helpDeskACurrentQueueNumber = 1;
+        helpDeskBCurrentQueueNumber = 1;
+        helpDeskCCurrentQueueNumber = 1;
+
+    }
+
+    public synchronized CentralizedQueuingSystem getInstance(){
+        if(instance == null)
+            instance = new CentralizedQueuingSystem();
+        return instance;
+    }
+
+    public synchronized void getCurrentQueueNumber(){
+
+        System.out.println("Current Queue Number:");
+        System.out.println("Help Desk A = A-" + helpDeskACurrentQueueNumber);
+        System.out.println("Help Desk B = B-" + helpDeskBCurrentQueueNumber);
+        System.out.println("Help Desk C = C-" + helpDeskCCurrentQueueNumber);
+
+    }
+
+    public synchronized void setCurrentQueueNumber(){
+        //Prints queue
+        int min = 1;
+        int max = 3;
+        int deskStation = random.nextInt(max - min + 1) + min;
+
+        switch(deskStation){
+            case 1:
+                System.out.println("A person entered the Help Desk A with a queue number: A-" + helpDeskACurrentQueueNumber);
+                helpDeskACurrentQueueNumber++;
+                break;
+            case 2:
+                System.out.println("A person entered the Help Desk B with a queue number: B-" + helpDeskBCurrentQueueNumber);
+                helpDeskBCurrentQueueNumber++;
+                break;
+            case 3:
+                System.out.println("A person entered the Help Desk C with a queue number: C-" + helpDeskCCurrentQueueNumber);
+                helpDeskCCurrentQueueNumber++;
+                break;
+        }
+    }
+
+    public synchronized void resetQueue(int deskStation, int currentNumber){
+
+        switch(deskStation){
+            case 1:
+                helpDeskACurrentQueueNumber = currentNumber;
+                break;
+            case 2:
+                helpDeskBCurrentQueueNumber = currentNumber;
+                break;
+            case 3:
+                helpDeskCCurrentQueueNumber = currentNumber;
+                break;
+        }
+    }
+}
---
+++ src/singletonPattern/PagIbigOfficeDeskStation.java
@@ -0,0 +1,69 @@
+package singletonPattern;
+
+import java.util.Scanner;
+
+public class PagIbigOfficeDeskStation {
+
+    public static void main (String args[]){
+
+        Scanner scan = new Scanner(System.in);
+
+        CentralizedQueuingSystem q = new CentralizedQueuingSystem();
+
+        System.out.println("PAG-IBIG OFFICE CENTRALIZED QUEUING SYSTEM\n");
+
+        while(true) {
+            System.out.println("[1] Generate New Queue List" +
+                    "\n[2] Current Queue Number (Online Monitoring)" +
+                    "\n[3] Reset Queue Number for a particular Desk Station" +
+                    "\n[4] Exit\n");
+
+            System.out.print("Enter Choice: ");
+            int choice = scan.nextInt();
+
+            if(choice == 1) {
+                System.out.println("\nCurrent Updated Queue List (20 persons only):");
+                for(int i = 0; i < 20; i++){
+                    q.setCurrentQueueNumber();
+                }
+                System.out.println();
+            }
+            else if(choice == 2){
+                System.out.println();
+                q.getCurrentQueueNumber();
+                System.out.println();
+            }
+            else if(choice == 3){
+                System.out.println();
+                System.out.println("Choose Desk Station and reset number");
+                System.out.print("Desk Station [A = 1][B = 2][C = 3] : ");
+                int deskStation = scan.nextInt();
+                System.out.print("Reset Number: ");
+                int resetNum = scan.nextInt();
+                q.resetQueue(deskStation, resetNum);
+                System.out.println();
+                q.getCurrentQueueNumber();
+                System.out.println();
+            }
+            else if (choice == 4) {
+                System.exit(0);
+                break;
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
